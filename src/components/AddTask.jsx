9f46dfd83919c073381a0b1a876afe6c27723b9e
ꜱᴛᴀʀᴋ <YeahAmStark@Gmail.com>
2024-02-28 14:12:12 +0530

    Best practise

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import { FiPlus } from "react-icons/fi";
 import { useDispatch } from "react-redux";
 import { addTask } from "../redux/taskSlice";
 import toast from "react-hot-toast";
 
 function AddTask() {
-  const [input, setInput] = useState("");
+  const inputRef = useRef(null);
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
-     e.preventDefault();
-    if (input === "") {
+    e.preventDefault();
+    const inputValue = inputRef.current.value.trim();
+    if (inputValue === "") {
       toast.error("Provide a task");
       return;
     }
-    dispatch(addTask(input));
-    setInput("");
+    dispatch(addTask(inputValue));
+    inputRef.current.value = ""; 
   };
-
 
   return (
     <form
@@ -29,11 +29,8 @@ function AddTask() {
       </button>
       <input
         type="text"
-        value={input}
+        ref={inputRef}
         placeholder="Add Your Task"
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
         className="focus:outline-none w-full h-3/5 rounded-md px-4 border border-slate-300 bg-transparent font-semibold"
       />
     </form>
